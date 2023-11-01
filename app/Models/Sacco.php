365bf7883981624e4c10ff2b52c67d6c55f36958
jay githiora <jaygithiora@gmail.com>
2023-11-01 11:48:36 +0300

    points

--- app/Models/Sacco.php
+++ app/Models/Sacco.php
@@ -9,7 +9,7 @@ class Sacco extends Model
 {
     use HasFactory;
     protected $fillable = ["name","slogan","phone", "status"];
-
+    protected $hidden = ["paybill", "passkey", "consumer_key", "consumer_secret"];
     public function mpesa_payment(){
         return $this->hasOne(MpesaPaymentSetting::class);
     }
