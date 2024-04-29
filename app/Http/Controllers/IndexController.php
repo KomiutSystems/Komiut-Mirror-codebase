e16fa2b1e18600bf4b65541705bbd4bd2d52b497
jay githiora <jaygithiora@gmail.com>
2024-04-29 19:45:12 +0300

    termini

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Gender;
+use App\Models\Place;
+use App\Models\Queue;
 use App\Models\SeatArrangement;
 use App\Models\Service;
+use App\Models\Terminus;
 use App\Models\Vehicle;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Yajra\DataTables\DataTables;
 
 class IndexController extends Controller
 {
@@ -46,5 +50,71 @@ class IndexController extends Controller
         } else {
             return response()->json(['loggedIn' => false]);
         }
+    }
+
+    public function termini(){
+        return view('termini');
+    }
+    public function getTermini(Request $request)
+    {
+
+        $termini = Terminus::with('place')->orderBy('name', 'ASC');
+
+        return DataTables::of($termini)
+            ->filter(function ($query) use ($request) {
+                $query->where('name', 'LIKE', '%' . $request->search . '%')
+                    ->where('status', 1);
+                    if($request->search_place > 0){
+                        $query->where('place_id', $request->search_place);
+                    }
+            })->editColumn('created_at', function ($row) {
+            return Carbon::parse($row->created_at)->diffForHumans();
+        })->addColumn('action', function ($row) {
+            $actionBtn = '<div style="white-space: nowrap;" class="text-end">' .
+                '<a href="' . url('termini/view/' . $row->id) . '" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i> View</a>'
+                . '</div>';
+            return $actionBtn;
+        })->addIndexColumn()->escapeColumns([])->make();
+    }
+    public function viewTerminus(Request $request){
+        $terminus = Terminus::with('place')->find($request->id);
+        if($terminus == null){
+            return redirect()->to('/');
+        }
+        return view('terminus', compact('terminus'));
+    }
+    public function getTerminusQueues(Request $request)
+    {
+
+        $queues = Queue::with('route.from', 'route.to', 'vehicle.sacco')->where('terminus_id', $request->id)->where('end_time', null)->orderBy('created_at', 'ASC');
+
+        return DataTables::of($queues)
+            ->filter(function ($query) use ($request) {
+                $query->whereHas('vehicle', function($query) use($request){
+                    $query->where('plate', 'LIKE', '%'.$request->search.'%');
+                });
+                if($request->from > 0){
+                    $query->whereHas('route', function($query) use($request){
+                        $query->where('from_id',$request->from);
+                    });
+                }
+                if($request->to > 0){
+                    $query->whereHas('route', function($query) use($request){
+                        $query->where('to_id',$request->to);
+                    });
+                }
+            })->editColumn('created_at', function ($row) {
+            return Carbon::parse($row->created_at)->diffForHumans();
+        })->addColumn('action', function ($row) {
+            $actionBtn = '<div style="white-space: nowrap;" class="text-end">' .
+                '<a href="' . url('pay_online?till_number=' . $row->vehicle->till_number) . '" class="btn btn-primary btn-sm"><i class="fas fa-wallet"></i> Pay</a>'
+                . '</div>';
+            return $actionBtn;
+        })->addIndexColumn()->escapeColumns([])->make();
+    }
+    public function searchPlaces(Request $request)
+    {
+        return json_encode(Place::where('name', 'LIKE', '%' . $request->q . '%')
+            ->skip(0)->take(5)->get());
     }
 }
---
+++ resources/views/termini.blade.php
@@ -0,0 +1,134 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class='container-fluid bg-primary'>
+    <div class='row d-flex align-items-center header-dark'>
+        <div class='col-sm-12 text-center mt-5 pt-5 pb-4 text-white'>
+            <h3 class='text-white'><i class='fas fa-map-marker-alt'></i> Termini</h3>
+        </div>
+    </div>
+</div>
+<div class='container mt-3'>
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-md-12 mb-3">
+                    <!-- small box -->
+                    <div class="card shadow-sm bg-white">
+                        <div class="card-header bg-white">
+                            <form class='search-form row d-flex align-items-end' id='search-form'>
+                                <div class="col mb-1">
+                                    <label>Search Name</label>
+                                    <input type="text" class="form-control" name="search" placeholder="Search">
+                                </div>
+                                <div class="col mb-1">
+                                    <label>Search Place</label>
+                                    <select class="form-control" name="search_place" id='places'>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                        <div class='card-body'>
+
+                            <div class="table-responsive">
+                                <table class='table w-100'>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Place</th>
+                                            <th class='text-end notexport'>Action</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- ./col -->
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
+@push('js')
+    <script>
+        $(document).ready(function() {
+            $('#places').select2({
+                theme: 'bootstrap-5',
+                width: '100%',
+                placeholder: 'Select Place',
+                //dropdownParent: $('#routeModal'),
+                allowClear: true,
+                ajax: {
+                    url: '{{ url('search/places') }}',
+                    dataType: 'json',
+                    delay: 250,
+                    processResults: function(data) {
+                        return {
+                            results: $.map(data, function(item) {
+                                return {
+                                    text: item.name,
+                                    id: item.id
+                                }
+                            })
+                        };
+                    },
+                    cache: true
+                }
+            });
+
+            var table = $('.table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ url('datatable/termini') }}",
+                    data: function(d) {
+                        d.search = $('.search-form input[name=search]').val();
+                        d.search_place = $('.search-form select[name=search_place]').val();
+                    }
+                },
+
+                "lengthMenu": [
+                    [20, 100, 250, 500, 1000],
+                    [20, 100, 250, 500, 1000]
+                ],
+                dom: "<'top'B>rt<'bottom'lip><'clear'>",//'lBtrip',
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'place.name',
+                        name: 'place.name'
+                    },{
+                        data: 'action',
+                        name: 'action',
+                        orderable: true,
+                        searchable: true
+                    },
+                ]
+            });
+            var timer = null;
+
+            $('#search-form input[type=text]').keyup('submit', function() {
+                clearTimeout(timer);
+                timer = setTimeout(function() {
+                    table.draw();
+                }, 1000);
+            });
+            $('#search-form select').change(function() {
+                table.draw();
+            });
+        });
+    </script>
+@endpush
