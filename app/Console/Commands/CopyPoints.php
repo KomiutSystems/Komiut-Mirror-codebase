9eac634e181618f3c398c199378d02c4f27b39c5
jay githiora <jaygithiora@gmail.com>
2024-11-14 16:13:34 +0300

    copy points debug

--- app/Console/Commands/CopyPoints.php
+++ app/Console/Commands/CopyPoints.php
@@ -34,8 +34,8 @@ class CopyPoints extends Command
         $point = Point::latest()->first();
         if ($point != null) {
             $url = "http://13.232.144.242/api/points/copy/from?created_at=" . urlencode($point->created_at);
+            \Log::info($point->created_at);
         }
-        \Log::info($point);
         //$created_at = Carbon::parse(urldecode(urlencode($qrcode_payment->created_at)));
         //return $created_at;
         $json = json_decode(file_get_contents($url), true);
