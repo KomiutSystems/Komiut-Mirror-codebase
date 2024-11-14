306b6a12525d41d72c98f8fc6104d43fa339ee49
jay githiora <jaygithiora@gmail.com>
2024-11-14 17:28:26 +0300

    copy point transactions

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends ConsoleKernel
         //$schedule->command('app:copy-queues')->everyTwoMinutes()->withoutOverlapping();
         //$schedule->command('app:copy-point-settings')->everyMinute();
         //$schedule->command('app:copy-points')->everyTwoMinutes();
+        $schedule->command('app:copy-point-transactions')->everyTwoMinutes();
         $schedule->command('app:generate-user-points')->everyTenMinutes()->withoutOverlapping();
         $schedule->command('app:generate-vehicle-summaries')->everyFiveMinutes()->withoutOverlapping();
         $schedule->command('app:check-passenger-payments')->everyTwoMinutes()->withoutOverlapping();
