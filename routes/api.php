d7736abf0cd779218e0dac17e8c16456e59eeda1
jay githiora <jaygithiora@gmail.com>
2024-11-14 09:18:14 +0300

    copy points

--- routes/api.php
+++ routes/api.php
@@ -58,6 +58,9 @@ Route::group([/*'middleware'=>['api']*/], function($router){
     Route::any('cashes/copy', [IndexApiController::class, 'copyCashTransactions']);
     Route::any('saccos/copy', [IndexApiController::class, 'copySaccos']);
 
+    Route::any('queues/points', [IndexApiController::class, 'copyPoints']);
+    Route::any('queues/points/from', [IndexApiController::class, 'copyPointsFrom']);
+
     Route::any('queues/copy', [IndexApiController::class, 'copyQueues']);
     Route::any('queues/copy/from', [IndexApiController::class, 'copyQueuesFrom']);
 
