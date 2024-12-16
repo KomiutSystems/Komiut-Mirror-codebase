82b7d2c48bdfe5304b373b6ee8a5640b260b7f5a
jay githiora <jaygithiora@gmail.com>
2024-12-16 11:51:02 +0300

    remove slave db

--- app/Models/FirebaseToken.php
+++ app/Models/FirebaseToken.php
@@ -9,7 +9,6 @@ class FirebaseToken extends Model
 {
     use HasFactory;
     protected $fillable = ["firebase_token", "device_id", "user_id"];
-
     public function user(){
         return $this->belongsTo(User::class);
     }
