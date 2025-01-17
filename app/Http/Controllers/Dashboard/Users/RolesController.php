2dfa47c1ad28cf913bb3d1ccc4a5fbc0f280c630
jay githiora <jaygithiora@gmail.com>
2025-01-17 07:58:53 +0300

    permissions read from master

--- app/Http/Controllers/Dashboard/Users/RolesController.php
+++ app/Http/Controllers/Dashboard/Users/RolesController.php
@@ -97,8 +97,8 @@ class RolesController extends Controller
                 return response()->json(['errors' => $validator->messages()], 400);
             }
 
-            $permissions = Permission::whereIn('id', $request->permissions != "" ? $request->permissions : [$request->permissions])->pluck("name");
-            $role = Role::where('id', $request->id)->first();
+            $permissions = Permission::on('mysql')->whereIn('id', $request->permissions != "" ? $request->permissions : [$request->permissions])->pluck("name");
+            $role = Role::on('mysql')->where('id', $request->id)->first();
             if ($role->syncPermissions($permissions)) {
                 return response()->json(['success' => 'Permissions updated successfully!']);
             } else {
