18501fcd303ae37d1060788f6f364741f5201834
jay githiora <jaygithiora@gmail.com>
2025-04-12 16:46:46 +0300

    checking

--- app/Http/Controllers/APIs/Dashboard/ExpenseAndFees/ExpenseAndFeesAPIController.php
+++ app/Http/Controllers/APIs/Dashboard/ExpenseAndFees/ExpenseAndFeesAPIController.php
@@ -7,6 +7,7 @@ use App\Models\VehicleExpenseAndFee;
 use App\Models\VehicleUser;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ExpenseAndFeesAPIController extends Controller
 {
@@ -23,7 +24,7 @@ class ExpenseAndFeesAPIController extends Controller
         $from_date = $request->date != "" ? Carbon::parse($request->date) : Carbon::today();
         $to_date = $from_date->copy()->addDays(1);
 
-        $vehicleExpenseFees = VehicleExpenseAndFee::with('vehicle.sacco', 'expense_fee')
+        $vehicleExpenseFees = VehicleExpenseAndFee::with('vehicle.sacco', 'vehicle.seat','expense_fee')
             ->whereBetween('trans_date', [$from_date, $to_date]);
 
         $vehicles = VehicleUser::where('user_id', auth()->user()->id)
@@ -47,110 +48,51 @@ class ExpenseAndFeesAPIController extends Controller
         })->skip($offset)->take(20)->get();
         return response()->json(['vehicle_expense_and_fees' => $vehicleExpenseFees]);
     }
-    public function addQueue(Request $request)
+    public function addVehicleExpenseAndFees(Request $request)
     {
-        if (auth()->user()->can('Add Queues') || auth()->user()->can('Edit Queues')) {
+        if (auth()->user()->can('Add Expense And Fees') || auth()->user()->can('Edit Expense And Fees')) {
             $validator = Validator::make($request->all(), [
                 'id' => 'required|integer|min:0',
+                'amount' => 'required|numeric|min:1',
                 'vehicle' => 'required|integer|exists:vehicles,id',
-                'terminus' => 'required|integer|exists:termini,id',
-                'status' => 'required|integer|exists:queue_statuses,id',
-                'route' => 'required|integer|exists:routes,id',
-                'choice' => 'required|integer|min:0|max:1',
-                'schedule_time' => 'required_if:choice,1',
-                'amount' => 'required|integer|min:1',
+                'trans_date' => 'required|date',
+                'expense_fee' => 'required|integer|exists:expense_fees,id',
+                'status' => 'required|integer|min:0|max:1'
             ]);
-
             if ($validator->fails()) {
                 return response()->json(['errors' => $validator->messages()], 400);
             }
-            if ($request->choice == 1) {
-                $now = Carbon::now('Africa/Nairobi');
-                $schedule_time = Carbon::parse($request->schedule_time);
-                if ($now > $schedule_time) {
-                    return response()->json(['error' => 'A future schedule time is required!']);
-                }
-            }
-            $route = Route::with(['from', 'to'])->where('id', $request->route)->first();
-            $terminus = Terminus::where('id', $request->terminus)->first();
-            if ($route->from_id != $terminus->place_id) {
-                return response()->json(['error' => 'Terminus has a different place from route'], 401);
-            }
-            if (
-                Queue::where('route_id', $request->route)-> /*where('queue_status_id', $request->status)->*/whereHas(
-                    'queue_status',
-                    function ($query) {
-                        $query->whereIn('status', ['Pending', 'Active']);
-                    }
-                )->where('vehicle_id', $request->vehicle)->where('id', '<>', $request->id)->count() > 0
-            ) {
-                $queueStatus = QueueStatus::where('status', 'Completed')->first();
-                if ($queueStatus != null) {
-                    Queue::where('route_id', $request->route)-> /*where('queue_status_id', $request->status)->*/whereHas(
-                        'queue_status',
-                        function ($query) {
-                            $query->whereIn('status', ['Pending', 'Active']);
-                        }
-                    )->where('vehicle_id', $request->vehicle)->where('id', '<>', $request->id)
-                    ->update(['queue_status_id' => $queueStatus->id, 'updated_at' => Carbon::now()]);
-                }else{
-                    return response()->json(['error' => 'Vehicle already queued'], 401);
-                }
-            }
-            $queue = new Queue();
+            $vehicleExpenseFee = new VehicleExpenseAndFee;
             if ($request->id > 0) {
-                $queue = Queue::findOrFail($request->id);
-            } else {
-                $qn = Queue::whereBetween('created_at', [Carbon::today(), Carbon::now()])
-                    ->where('terminus_id', $request->terminus)->where('route_id', $request->route)->count() + 1;
-                $queue->queue_number = 'QN-' . $qn;
+                $vehicleExpenseFee = VehicleExpenseAndFee::findOrFail($request->id);
             }
+            $vehicleExpenseFee->vehicle_id = $request->vehicle;
+            $vehicleExpenseFee->expense_fee_id = $request->expense_fee;
+            $vehicleExpenseFee->amount = $request->amount;
+            $vehicleExpenseFee->trans_date = Carbon::parse($request->trans_date);
+            $vehicleExpenseFee->status = $request->status;
 
-            $queue->vehicle_id = $request->vehicle;
-            $queue->terminus_id = $request->terminus;
-            $queue->route_id = $request->route;
-            $queue->queue_status_id = $request->status;
-            if ($request->choice == 1) {
-                $queue->schedule_time = Carbon::parse($request->schedule_time);
-                $queue->start_time = Carbon::parse($request->schedule_time);
-            } else {
-                $queue->start_time = Carbon::now();
-            }
-            $queue->queue_type = $request->choice;
-            $queue->amount = $request->amount;
-            $queue->user_id = Auth::user()->id;
-            if ($queue->save()) {
-                $stages = RouteStage::where('route_id', $request->route)->get();
-                foreach ($stages as $stage) {
-                    if (QueuePlace::where('queue_id', $queue->id)->where('route_stage_id', $stage->id)->count() == 0) {
-                        $queuePlace = new QueuePlace;
-                        $queuePlace->queue_id = $queue->id;
-                        $queuePlace->route_stage_id = $stage->id;
-                        $queuePlace->save();
-                    }
+            if ($vehicleExpenseFee->save()) {
+                $summary = Summary::where('trans_date', Carbon::parse($request->trans_date)->format('Y-m-d'))
+                    ->where('vehicle_id', $request->vehicle_id)->first();
+                if ($summary == null) {
+                    $summary = new Summary;
+                    $summary->mpesa_amount = 0;
+                    $summary->cash_amount = 0;
+                    $summary->mpesa_txn = 0;
+                    $summary->cash_txn = 0;
+                    $summary->vehicle_id = $request->vehicle;
+                    $summary->trans_date = Carbon::parse($request->trans_date)->format('Y-m-d');
                 }
-                /*$tokens = FirebaseToken::whereHas('user.vehicle_users', function($query) use($request){
-                    $query->where('vehicle_id', $request->vehicle);
-                })->pluck('firebase_token');*/
-                $tokens = FirebaseToken::whereHas('user', function ($userQuery) use ($request) {
-                    $userQuery->whereHas('vehicle_users', function ($q) use ($request) {
-                        $q->where('vehicle_id', $request->vehicle);
-                    })->whereHas('permissions', function ($q) {
-                        $q->where('name', 'Get Queue Notifications'); // 👈 check specific permission name
-                    });
-                })->pluck('firebase_token');
-                $vehicle = Vehicle::find($request->vehicle);
-                $title = $vehicle->plate." Queued";
-                $message = $vehicle->plate." queued for ".$route->from->name." to ".$route->to->name;
-                foreach($tokens as $token){
-                    dispatch(new SendFCMJob($token, $title, $message, 'queues_screen', 0));
-                }
-                return response()->json(['success' => "Queue updated successfully!"]);
+                $summary->expense_fee_amount = VehicleExpenseAndFee::where('vehicle_id', $request->vehicle)
+                    ->where('trans_date', Carbon::parse($request->trans_date)->format('Y-m-d'))->sum('amount');
+                $summary->save();
+                return response()->json(['success' => 'Vehicle Expense & Fee updated successfully!']);
             } else {
-                return response()->json(['error' => 'Unable to update queue'], 401);
+                return response()->json(['error' => 'Unable to update Vehicle Expense & Fee'], 401);
             }
         } else {
-            return response()->json(['error' => 'You do not have permissione to Add/Edit Queues'], 401);
+            return response()->json(['error' => 'You do not have permissions to Add/Edit Vehicle Expense And Fees'], 401);
         }
     }
 }
---
+++ app/Http/Controllers/APIs/Dashboard/Settings/ExpenseAndFeesSettingsAPIController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\APIs\Dashboard\Settings;
+
+use App\Http\Controllers\Controller;
+use App\Models\ExpenseFee;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ExpenseAndFeesSettingsAPIController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth:sanctum');
+    }
+    public function index(Request $request){
+
+        $page = $request->has('page') ? intval($request->page) : 1;
+        $page--;
+        $offset = $page * 20;
+
+        $expense_and_fees = ExpenseFee::with("sacco");
+        if($request->sacco > 0){
+            $expense_and_fees  = $expense_and_fees->where("sacco_id", $request->sacco);
+        }
+
+        $expense_and_fees = $expense_and_fees->where('name', 'LIKE', '%'.$request->search.'%')
+        ->orderBy('name', 'ASC')->skip($offset)->take(20)->get();
+        return response()->json(['expense_and_fees'=>$expense_and_fees]);
+    }
+
+
+    public function addExpenseFee(Request $request)
+    {
+        if (auth()->user()->can('Add Expense And Fees Settings') || auth()->user()->can('Edit Expense And Fees Settings')) {
+            $validator = Validator::make($request->all(), [
+                'id' => 'required|integer|min:0',
+                'name' => 'required|string',
+                'sacco' => 'nullable|integer',
+                'type'=>'required|string',
+                'status' => 'required|integer|max:1,min:0'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->messages()], 400);
+            }
+            if(ExpenseFee::where('id', '<>', $request->id)->where('name', $request->name)->where('sacco_id', $request->sacco)->count() > 0){
+                return response()->json(['error'=>'Expense/Fee already exists!'], 401);
+            }
+            $expenseFee = new ExpenseFee();
+            if ($request->id > 0) {
+                $expenseFee = ExpenseFee::findOrFail($request->id);
+            }
+
+            $expenseFee->name = $request->name;
+            $expenseFee->type= $request->type;
+            $expenseFee->sacco_id = $request->sacco;
+            $expenseFee->status = $request->status;
+            if ($expenseFee->save()) {
+
+                return response()->json(['success' => "Expense And Fee updated successfully!"]);
+            } else {
+                return response()->json(['error' => 'Unable to update expense/fee'], 401);
+            }
+        } else {
+            return response()->json(['error' => 'You do not have permissione to Add/Edit Expense/Fee'], 401);
+        }
+    }
+
+}
