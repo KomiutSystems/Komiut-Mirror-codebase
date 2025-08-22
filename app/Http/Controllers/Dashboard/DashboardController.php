58de51e8ab1121d866f52cea2f36f06140ce5126
Eric Muthemba <diysolutions16@gmail.com>
2025-08-22 14:19:57 +0300

    komiut

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Dashboard;
+use Illuminate\Support\Str;
 
 use App\Http\Controllers\Controller;
 use App\Models\Sacco;
--- app/Http/Controllers/Dashboard/Vehicles/VehicleController.php
+++ app/Http/Controllers/Dashboard/Vehicles/VehicleController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Dashboard\Vehicles;
+use Illuminate\Support\Str;
 
 use App\Http\Controllers\Controller;
 use App\Models\Sacco;
